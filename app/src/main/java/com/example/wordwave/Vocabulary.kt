d00fdaed560ccee02ab2281cd21d2346121e82f0
nnetr <nnetreba06@gmail.com>
2025-03-03 01:24:02 +0300

    add vocabulary screen

---
+++ app/src/main/java/com/example/wordwave/Vocabulary.kt
@@ -0,0 +1,20 @@
+package com.example.wordwave
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun VocabularyScreen(){
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun VocabularyPreview(){
+    VocabularyScreen()
+}
+
+
+
+
+
