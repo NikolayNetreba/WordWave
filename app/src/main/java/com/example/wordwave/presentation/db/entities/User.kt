76ba925baddb4e0b552939858ae7c177c1a83438
JIvot <alkurlaev@yandex.ru>
2025-05-17 19:56:25 +0300

    Removed everything

--- app/src/main/java/com/example/wordwave/presentation/db/entities/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wordwave.presentation.db.entities
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-data class User(
-    @PrimaryKey val id: String,
-)
